2161078d8b8eb4a7277705a90ca76c9f54591181
gmnagy@gmail.com <gmnagy@gmail.com@4107d651-a4c8-415a-9f25-286c730fa24a>
2011-06-18 13:09:23 +0000

    csak gepet valtottam

---
+++ src/main/java/hu/nsmdmp/distributions/TruncBivPoissArray.java
@@ -0,0 +1,20 @@
+package hu.nsmdmp.distributions;
+
+public class TruncBivPoissArray {
+	public static double[][] getTruncBivPoissArray(final double lambda, final double lambda1, final double lambda2,
+			final int maxOccur1,final int maxOccur2 ){
+		double[][] result=new double[maxOccur1+1][maxOccur2+1];
+		int maxOccur=Math.max(maxOccur1, maxOccur2);
+		for(int i=0; i<=maxOccur; i++){
+			for(int i2=0; i2<=maxOccur2;i2++){
+				for(int i1=0; i1<=maxOccur1; i1++){
+					result[Math.min(i+i1,maxOccur1)][Math.min(i+i2,maxOccur2)]=0;
+				}
+			}
+		}
+		return result;
+		
+	}
+
+
+}
