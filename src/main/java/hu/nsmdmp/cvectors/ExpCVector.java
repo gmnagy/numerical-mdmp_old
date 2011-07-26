275436a7d6b60bac5d6ec707359da26548f4fe10
gmnagy@gmail.com <gmnagy@gmail.com@4107d651-a4c8-415a-9f25-286c730fa24a>
2011-07-26 12:41:12 +0000

    example 5 is stimmel. Ezt a Zoli irta el :)

--- src/main/java/hu/nsmdmp/cvectors/ExpCVector.java
+++ src/main/java/hu/nsmdmp/cvectors/ExpCVector.java
@@ -9,6 +9,8 @@ public class ExpCVector extends AbstractCVector {
 
 	/**
 	 * f(z1,z2) = Exp[z1/50 + z2/50 + z1z2/10000].
+	 * helyett
+	 * f(z1,z2) = Exp[z1/50 + z2/200 + z1z2/10000].
 	 */
 	@Override
 	Apfloat function(final Apfloat[] variation) {
@@ -19,11 +21,13 @@ public class ExpCVector extends AbstractCVector {
 		Apfloat z1 = variation[0];
 		Apfloat z2 = variation[1];
 
-		// (z1+z2)/50
-		Apfloat a = z1.add(z2).divide(new Apfloat(50d, Precision.SCALE));
-		// (z1*z2)/10000
-		Apfloat b = z1.multiply(z2).divide(new Apfloat(10000d, Precision.SCALE));
+		// z1/25
+		Apfloat a = z1.divide(new Apfloat(50d, Precision.SCALE));
+		// z2/400
+		Apfloat b = z2.divide(new Apfloat(200d, Precision.SCALE));
+		// (z1*z2)/50000
+		Apfloat c = z1.multiply(z2).divide(new Apfloat(10000d, Precision.SCALE));
 
-		return ApfloatMath.exp(a.add(b));
+		return ApfloatMath.exp(a.add(b).add(c));
 	}
 }
